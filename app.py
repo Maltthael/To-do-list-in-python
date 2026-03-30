6b60b5b0a98bce8e44acfa397207443e81727183
Maltthael <macrodrigues01@hotmail.com>
2026-03-30 14:57:01 -0300

    creating features to meta page

--- app.py
+++ app.py
@@ -1,54 +1,68 @@
 from flask import Flask, render_template, redirect, request
 from markupsafe import escape
 
-
 app = Flask(__name__)
 
+# Listas globais para armazenar os dados
 checklists = []
 metas = []
 
 @app.route('/')
 def home():
-    return render_template('index.html', checklists = checklists)
+    return render_template('index.html', checklists=checklists)
 
 @app.route('/create_checklist')
 def create():
     new_checklist = []
-    checklists.append(new_checklist) #create a empty checklist
+    checklists.append(new_checklist) # cria uma checklist vazia
     return redirect('/')
 
-@app.route('/add_task/<int:list_id>', methods = ['POST'])
+@app.route('/add_task/<int:list_id>', methods=['POST'])
 def add_task(list_id):
     task_text = request.form['task']
+<<<<<<< HEAD
     if task_text.strip() != " ":
         checklists[list_id].append(task_text) # Adiciona uma tarefa dentro da checklist(Precisa ter um nome)
+=======
+    if task_text.strip() != "":
+        checklists[list_id].append(task_text) 
+>>>>>>> 6fc4cf0 (creating features to meta page)
     return redirect('/')
-
 
 @app.route("/delete_task/<int:list_id>/<int:task_id>")
 def delete_task(list_id, task_id):
     checklists[list_id].pop(task_id)
     return redirect("/")
 
+# --- SEÇÃO DE METAS ---
 
+<<<<<<< HEAD
 @app.route('/meta')
-def page_meta():
-    return render_template('meta.html', metas = metas)
+=======
 
+@app.route('/page_meta')
+>>>>>>> 6fc4cf0 (creating features to meta page)
+def page_meta():
+    return render_template('meta.html', metas=metas)
 
 @app.route('/create_meta')
 def create_m():
     new_meta = []
-    metas.append(new_meta) #create an empty meta
-    return redirect('/meta')
+    metas.append(new_meta) # cria uma meta vazia
+    return redirect('/page_meta')
 
-
-@app.route('/add_task_meta/<int:list_meta_id>', methods = ['POST'])
-def add_task_meta(list_meta_id):
+@app.route('/add_task_meta/<int:meta_id>', methods=['POST'])
+def add_task_meta(meta_id):
     task_text = request.form['task_meta']
-    if task_text.strip() != " ":
-        metas[list_meta_id].append(task_text)
-    return redirect('/meta')
+    if task_text.strip() != "":
+        metas[meta_id].append(task_text)
+    return redirect('/page_meta')
+
+
+@app.route('/delete_task_meta/<int:meta_id>/<int:task_meta_id>')
+def delete_task_meta(meta_id, task_meta_id):
+    metas[meta_id].pop(task_meta_id)
+    return redirect('/page_meta')
 
 
 if __name__ == "__main__":
